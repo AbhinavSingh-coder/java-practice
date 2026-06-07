e4b20723cdc627d30378efad564e88b19e7e6c0d
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-07 17:11:08 +0530

    Question on do while loop

---
+++ loops/question8.java
@@ -0,0 +1,12 @@
+//print hello world using do while loop.
+
+public class question8 {
+    public static void main(String[] args) {
+       int counter = 0;
+       do{
+        System.out.println("hello world");
+        counter++;
+       }while(counter<=10);
+    }
+    
+}
