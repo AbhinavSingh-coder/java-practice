81822316fbba9504add814f83f93f8e210f29df7
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-09 21:19:09 +0530

    added some question

---
+++ functions/question1.java
@@ -0,0 +1,15 @@
+//product of java(a=3, b=4)
+
+class question1{
+    public static int multiply(int a, int b){
+        int product = a*b;
+       
+     return product;
+    }
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+      int prod = multiply(a , b);
+       System.out.println(prod);
+    }
+}
