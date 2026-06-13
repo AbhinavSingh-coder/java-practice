97d1aaf659851f67d77e38e419ebd81e7a35e540
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-13 17:52:26 +0530

    half pyramid

---
+++ advance pattern/question2.java
@@ -0,0 +1,21 @@
+//half pyramid
+import java.util.*;
+public class question2 {
+    public static void half_pyramid(int n){
+      for(int i =1;i<=n;i++){
+        for(int j =1;j<=n-i;j++){
+            System.out.print(" ");}
+            for(int j =1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+      }
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your no. ");
+        int n = sc.nextInt();
+        half_pyramid(n);
+      }
+    }
+
