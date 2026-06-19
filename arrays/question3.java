e278a3210671d90096bea2b5631b6fc3b9c54e24
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-19 08:46:38 +0530

    buy and sell profit question

---
+++ arrays/question3.java
@@ -0,0 +1,19 @@
+public class question3 {
+    public static int stocks(int prices[]){
+        int buyprice = Integer.MAX_VALUE;
+        int maxprofit = 0;
+
+        for(int i=0;i<prices.length;i++){
+           if(buyprice<prices[i]){// price[i] = sp
+           int profit= prices[i]-buyprice;//today profit
+           maxprofit= Math.max(maxprofit,profit);
+            }else{
+                  buyprice = prices[i];
+            }
+        }return maxprofit;
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+       System.out.println(stocks(prices)); 
+    }
+}
