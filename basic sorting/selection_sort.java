dc554eccd122dad464d46bccef3a1f4ac0c3da5a
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-21 15:56:26 +0530

    selection sorting

---
+++ basic sorting/selection_sort.java
@@ -0,0 +1,25 @@
+
+
+public class selection_sort {
+    public static void sorting(int arr[]){
+        for(int i =0;i<arr.length-1;i++){          
+            int min = i;         
+            for(int j=i+1;j<arr.length;j++){
+            if(arr[min]>arr[j]){             
+                min= j;
+            } 
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i] + " ");
+        }System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        sorting(arr);
+        
+    }
+}
