8263631b5f410c81f6b5df14ffeabc5da2ce0126
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-24 14:14:23 +0530

    .....

---
+++ Questions/q7.java
@@ -0,0 +1,14 @@
+   import java.util.*;
+   class q7{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter your no. ");
+        int n = sc.nextInt();
+        int i =1;
+        while(i<=n){
+            System.out.println(i);
+            i++;
+        }
+
+    }
+   }
