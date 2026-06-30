2e5d37bd72094b0413747658b76cee1206419a50
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-06-30 21:32:56 +0530

    .....

---
+++ bit manipulation/q4.java
@@ -0,0 +1,10 @@
+public class q4 {
+    public static int clearIthbit(int n, int i){
+        int bitmask = ~(1<<i);
+        return n&bitmask;
+    }
+    public static void main(String[] args) {
+       System.out.println(clearIthbit(10, 1)); 
+        
+    }
+}
