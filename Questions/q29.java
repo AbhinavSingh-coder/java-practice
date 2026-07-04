1ff1f3b2370467522fab1358c08eee866eede152
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-04 23:54:52 +0530

    ....

---
+++ Questions/q29.java
@@ -0,0 +1,16 @@
+public class q29 {
+    public static boolean repeat(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j =i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]){
+                    return true;
+                }
+            }
+        }return false;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,1};
+        System.out.println(repeat(arr));
+    }
+    
+}
