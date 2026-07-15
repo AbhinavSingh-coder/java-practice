26b210c280950882d922b2714fcf7d7488d88c9d
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-15 22:07:59 +0530

    sum of n no.

---
+++ recursion basics/q4.java
@@ -0,0 +1,18 @@
+
+
+public class q4 {
+    public static int sum(int n){
+        if(n ==1){
+            return 1;
+        }
+        int sum1 = sum(n-1);
+        int sum = n +sum1;
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(sum(n));
+        
+    }
+    
+}
