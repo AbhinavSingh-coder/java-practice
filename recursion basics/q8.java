0c339859c27b8e05e9f5783cac650ad25043c754
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-17 22:35:44 +0530

    find last occurence

---
+++ recursion basics/q8.java
@@ -0,0 +1,17 @@
+public class q8 {
+    public static int last(int arr[],int key,int i){
+        if(i == arr.length){
+            return -1;
+        }
+      int found =  last(arr,key,i+1);
+        if(found == -1 && arr[i] == key){
+            return i;
+        }return found;
+    }
+    public static void main(String[] args) {
+         int arr[] = {2,4,5,6,7,8,5,9};
+        int key =5;
+        System.out.println(last(arr, key, 0));
+    }
+    
+}
