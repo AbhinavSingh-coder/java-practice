1a429e455a1a2d1bb2992ffb0f92640dbebf420d
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-24 21:25:04 +0530

    object

---
+++ oops/object.java
@@ -0,0 +1,26 @@
+class object{
+public static void main(String[] args) {
+    Pen p1 = new Pen();
+    p1.setcolor("blue");
+    System.out.println(p1.color);
+
+    p1.settip(5);
+    System.out.println(p1.tip);
+    
+}
+}
+    
+class Pen{
+    String color;
+    int tip;
+
+
+    void setcolor(String newcolor){
+        color = newcolor;
+    }
+    void settip(int newtip){
+        tip = newtip;
+    }
+
+}
+
