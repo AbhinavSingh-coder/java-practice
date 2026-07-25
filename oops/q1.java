d1afaca0ae896360680e15a59e76eb3d44e7cf87
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-25 23:07:59 +0530

    ...

---
+++ oops/q1.java
@@ -0,0 +1,13 @@
+public class q1 {
+    public static void main(String[] args) {
+        Student c = new Student();
+        c.name = "aman";
+        System.out.println(c.name);
+    }
+    
+}
+
+class Student{
+    String name;
+    int marks;
+}
