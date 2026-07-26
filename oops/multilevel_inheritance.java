65a853dbc5b2aa89637d46b103ba6c1e76b728a0
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-26 22:11:52 +0530

    multilevel inheritance

---
+++ oops/multilevel_inheritance.java
@@ -0,0 +1,30 @@
+public class multilevel_inheritance {
+    public static void main(String[] args) {
+        Dog dobby = new Dog();
+        dobby.eat();
+        dobby.legs = 4;
+        System.out.println(dobby.legs);
+
+        
+    }
+    
+}
+
+class Animal{
+    String color;
+    void eat(){
+        System.out.println("eats");
+    }
+
+    void breathe(){
+        System.out.println("breathes");
+    }
+}
+
+class Mammel extends Animal{
+    int legs;
+}
+
+class Dog extends Mammel{
+   String breed;
+}
