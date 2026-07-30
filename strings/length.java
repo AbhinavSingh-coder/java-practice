26b838e877da3909baf1eacda66119491168e377
AbhinavSingh-coder <thakurabhinavsingh29@gmail.com>
2026-07-30 23:32:38 +0530

    length in string

---
+++ strings/length.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class length {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+     System.out.print("enter your name ");
+         String name = sc.nextLine();
+          System.out.println(name);
+          System.out.println("the length of name is " + name.length());
+
+}
+}
